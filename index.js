e5d9119daaffd4fd81de14da7cf407ce91c213ca
elias <elias.e.solis@gmail.com>
2023-08-16 13:13:48 -0300

    nuevas cosas

--- index.js
+++ index.js
@@ -40,37 +40,61 @@ const todos = document.getElementById("Todos")
 const mujer = document.getElementById("Mujeres")
 const hombre = document.getElementById("Hombres")
 
+let datapersonajes = {};
+
+const printData = arr => {
+  for (let i = 0; i<arr.length; i++){
+    /*  console.log(arr[i].name) */
+     let card = document.createElement("div")
+     card.classList.add("card")
+     let nombre = document.createElement("p")
+         nombre.innerText= "Nombre : " + arr[i].name
+         card.appendChild(nombre)
+         let img = document.createElement("img")
+         img.src=arr[i].image
+         card.appendChild(img)
+         let status = document.createElement("p")
+         status.innerText="Status : " +  arr[i].status
+         card.appendChild(status)
+         let species = document.createElement("p")
+         species.innerText="Especies : " +arr[i].species
+         card.appendChild(species)
+         let gender = document.createElement("p")
+         gender.innerText= "Genero : " + arr[i].gender
+         card.appendChild(gender)
+         let origin = document.createElement("p")
+         origin.innerText="Origen : " +  arr[i].origin.name
+         card.appendChild(origin)
+         let location = document.createElement("p")
+         location.innerText="Location : "  + arr[i].location.name
+         card.appendChild(location)
+         container.appendChild(card)
+     }  
+} 
+
  async function getPersonajes() {
     let response = await fetch("https://rickandmortyapi.com/api/character"
     )
     let data = await response.json();
 rickAndMortyPersonajes=data.results;
-for (let i = 0; i<rickAndMortyPersonajes.length; i++){
-    console.log(rickAndMortyPersonajes[i].name)
-    let card = document.createElement("div")
-    card.classList.add("card")
-    let nombre = document.createElement("p")
-        nombre.innerText=rickAndMortyPersonajes[i].name
-        card.appendChild(nombre)
-        let img = document.createElement("img")
-        img.src=rickAndMortyPersonajes[i].image
-        card.appendChild(img)
-        let status = document.createElement("p")
-        status.innerText=rickAndMortyPersonajes[i].status
-        card.appendChild(status)
-        let species = document.createElement("p")
-        species.innerText=rickAndMortyPersonajes[i].species
-        card.appendChild(species)
-        let gender = document.createElement("p")
-        gender.innerText=rickAndMortyPersonajes[i].gender
-        card.appendChild(gender)
-        let origin = document.createElement("p")
-        origin.innerText=rickAndMortyPersonajes[i].origin.name
-        card.appendChild(origin)
-        let location = document.createElement("p")
-        location.innerText=rickAndMortyPersonajes[i].location.name
-        card.appendChild(location)
-        container.appendChild(card)
-    }     
+datapersonajes=data.results
+   printData(datapersonajes)
 }
 getPersonajes()
+
+mujer.addEventListener("click", () => {
+  const arr = datapersonajes;
+  let mujeres = [];
+  for (let i = 0; i < arr.length; i++) {
+    if(arr[i].gender === 'Female'){
+      mujeres.push(arr[i])
+    }
+    }
+   printData(mujeres)
+})
+
+hombre.addEventListener("click", () =>{
+  const arr =  datapersonajes;
+  let hombres = arr.filter(personaje => personaje.gender === "Male")
+  printData(hombres)
+})
